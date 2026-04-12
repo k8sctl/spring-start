7ecbebfdaf1b14c8903a046642b512900dc1e42a
thbins <joons0218@gmail.com>
2026-04-12 22:26:44 +0900

    학습: Spring 핵심 기능 학습 진행 중
    - 프로젝트 초기 세팅
    - 빈(Bean) 등록 및 의존성 주입(DI) 실습
    - JDBC 및 JdbcTemplate 활용

---
+++ src/main/java/hello/hello_spring/SpringConfig.java
@@ -0,0 +1,37 @@
+package hello.hello_spring;
+
+import hello.hello_spring.repository.JdbcMemberRepository;
+import hello.hello_spring.repository.JdbcTemplateMemberRepository;
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+import hello.hello_spring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import javax.swing.*;
+import javax.xml.crypto.Data;
+
+@Configuration
+public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        // return new MemberRepository();
+        // return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
+    }
+}
